97365008dbc7eed22c644a9aaa9edfdaac2f3a12
Murad R <90379851+sobaking3@users.noreply.github.com>
2021-09-21 13:55:52 +0300

    Add files via upload

---
+++ 2isp20/Ragimov/PR2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PR2
+{
+    class RUN
+    {
+        static void Main()
+
+        {
+            double x = 3.14, y = 17.25;
+
+            double add, sub, mul, div;
+
+            add = x + y;
+
+            sub = x - y;
+
+            mul = x * y;
+
+            div = x / y;
+
+            Console.Write("add = " + add + "\n\n" +
+                     "sub = " + sub + "\n\n" +
+                     "mul = " + mul + "\n\n" +
+                     "div = " + div);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2isp20/Ragimov/PR5/Program.cs
@@ -0,0 +1,23 @@
+﻿class Person
+{
+    public string name; // имя
+    public int age;     // возраст
+
+    public void GetInfo()
+    {
+        System.Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Person();
+        tom.GetInfo();      // Имя: Возраст: 0
+
+        tom.name = "Tom";
+        tom.age = 34;
+        tom.GetInfo();  // Имя: Tom Возраст: 34
+        System.Console.ReadKey();
+    }
+}
